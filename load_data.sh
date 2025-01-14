a122d47a6730806de66dd9f015b0eb851db771f2
rayondemiel <maximehumeau44@gmail.com>
2025-01-14 15:02:57 +0100

    Update load_data.sh

--- load_data.sh
+++ load_data.sh
@@ -9,17 +9,17 @@ cd $ARCHESPATH
 python manage.py packages -o setup_db
 
 # Load Liverpool specific Arches defaut thesauri and collections
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/concepts/aat_thesaurus.xml -ow overwrite -st keep
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/concepts/agorha_material_description_characteristics.xml -ow overwrite -st keep
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/concepts/agorha_materials.xml -ow overwrite -st keep
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/concepts/agorha_techniques.xml -ow overwrite -st keep
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/concepts/agorha_type_of_material_description.xml -ow overwrite -st keep
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/concepts/agorha_type_of_work.xml -ow overwrite -st keep
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/concepts/database_manuspectrum_concept.xml -ow overwrite -st keep
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/concepts/frollo_analysis_techniques.xml -ow overwrite -st keep
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/concepts/loterre_concepts.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/aat_thesaurus.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/agorha_material_description_characteristics.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/agorha_materials.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/agorha_techniques.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/agorha_type_of_material_description.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/agorha_type_of_work.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/database_manuspectrum_concept.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/frollo_analysis_techniques.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/loterre_concepts.xml -ow overwrite -st keep
 
-python manage.py packages -o import_reference_data -s $MSPATH/reference_data/collections/collections.xml -ow overwrite -st keep
+python manage.py packages -o import_reference_data -s $MSPATH/reference_data/collections.xml -ow overwrite -st keep
 
 # Load Resource Models, Branches
 python manage.py packages -o import_graphs -s arches/db/graphs/branches
